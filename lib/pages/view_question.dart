fca7017ad86178533b5cb055783feec10565d840
Sean <dev@nyayozangu.com>
2018-11-07 20:34:50 +0300

    mycar v0.1.7 question view with a better question changes: - changed the question view in view question to expansion tile

--- lib/pages/view_question.dart
+++ lib/pages/view_question.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:my_car/models/answer.dart';
 import 'package:my_car/models/main_model.dart';
 import 'package:my_car/models/question.dart';
+import 'package:my_car/models/user.dart';
 import 'package:my_car/utils/strings.dart';
 import 'package:my_car/views/answer_item.dart';
-import 'package:my_car/views/question_item.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 const tag = 'ViewQuestionPage';
@@ -16,11 +16,48 @@ class ViewQuestionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _questionSection = QuestionItemView(
-      question: question,
+    final createdBy =
+    question.createdBy != null ? question.createdBy : question.userId;
+    final _userIcon = Icon(
+      Icons.account_circle,
+      color: Colors.grey,
+      size: 18.0,
+    );
+    final _questionSection = Material(
+      elevation: 4.0,
+      child: ExpansionTile(
+        leading: ScopedModelDescendant<MainModel>(
+          builder: (_, __, model) {
+            return FutureBuilder<User>(
+                future: model.getUserFromUserId(createdBy),
+                builder: (_, snapshot) {
+                  if (!snapshot.hasData) return _userIcon;
+                  final user = snapshot.data;
+                  return user.imageUrl != null
+                      ? CircleAvatar(
+                    backgroundColor: Colors.black12,
+                    backgroundImage: NetworkImage(user.imageUrl),
+                  )
+                      : _userIcon;
+                });
+          },
+        ),
+        title: Text('${question.question.substring(0, 35)}...'),
+        children: <Widget>[
+          ListTile(
+            title: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                question.question,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+            ),
+          )
+        ],
+      ),
     );
 
-    var _answersSection = ScopedModelDescendant<MainModel>(
+    final _answersSection = ScopedModelDescendant<MainModel>(
       builder: (_, __, model) {
         return StreamBuilder(
             stream: model.answersStreamFor(question),
